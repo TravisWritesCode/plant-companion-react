859d248ddb033dba14edb246d468936b940b5787
kbr01 <katbranham2@gmail.com>
2021-03-31 15:46:14 -0400

    Updated UI with more readable data

--- src/components/Pot.js
+++ src/components/Pot.js
@@ -1,13 +1,21 @@
 import React, { Component, Fragment, useEffect }  from 'react';
 import { Link } from 'react-router-dom';
 
-export default class PotAdmin extends Component { 
-  
+export default class PotAdmin extends Component {
+    soilMoistureOutput;
   render() { 
     const data = this.props
+      if (this.props.soilMoisture > 21500)
+        this.soilMoistureOutput = "Dry"
+      else if (this.props.soilMoisture <= 21500 && this.props.soilMoisture > 17000)
+          this.soilMoistureOutput = "Ideal"
+      else if (this.props.soilMoisture <= 17000 && this.props.soilMoisture > 16000)
+          this.soilMoistureOutput = "Wet"
+      else
+          this.soilMoistureOutput = "Over-Watered"
     console.log(data)
+
     return (
-      
       <div className="plant plantPhoto">
         <div>
           <Link className="PlantName" to={{pathname:"/potHistory", state: data}}>{this.props.potName}</Link>
@@ -15,7 +23,7 @@ export default class PotAdmin extends Component {
           <div className="PlantLevels">
               <h3>Temperature: {this.props.temp}&deg;F</h3>
               <h3>Reservoir Level: {this.props.reservoirLevel}</h3>
-              <h3>Moisture: {this.props.soilMoisture}</h3>
+              <h3>Moisture: {this.soilMoistureOutput}</h3>
               <h3>Sun: {this.props.photosensor}</h3>
           </div>
         </div>
--- src/components/PotHistoryData.js
+++ src/components/PotHistoryData.js
@@ -1,17 +1,29 @@
 import React, { Component, Fragment, useEffect }  from 'react';
 
-export default class PotHistoryData extends Component { 
+export default class PotHistoryData extends Component {
+  soilMoistureOutput;
   render() { 
     const data = this.props.sensorData
+    const dateData = this.props.timestamp
+    const dateObject = new Date(dateData * 1000)
+    const dateOutput = dateObject.toLocaleString()
+      if (this.props.soilMoisture > 21500)
+        this.soilMoistureOutput = "Dry"
+      else if (this.props.soilMoisture <= 21500 && this.props.soilMoisture > 17000)
+          this.soilMoistureOutput = "Ideal"
+      else if (this.props.soilMoisture <= 17000 && this.props.soilMoisture > 16000)
+          this.soilMoistureOutput = "Wet"
+      else
+          this.soilMoistureOutput = "Over-Watered"
     console.log(data)
     return (
       <div className="potData">
         <div>
           <div className="PotHistoryEntry">
-            <h3>Time: {this.props.timestamp}</h3>
+            <h3>Date: {dateOutput}</h3>
             <h3>Temperature: {this.props.temp}&deg;F</h3>
             <h3>Reservoir Level: {this.props.reservoirLevel}</h3>
-            <h3>Moisture: {this.props.soilMoisture}</h3>
+            <h3>Moisture: {this.soilMoistureOutput}</h3>
             <h3>Sun: {this.props.photosensor}</h3>
           </div>
         </div>
