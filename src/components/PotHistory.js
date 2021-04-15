5900134d34eb7325549fdd2d34fb50f0d58f7ccc
kbr01 <katbranham2@gmail.com>
2021-04-15 16:53:09 -0400

    pagination

--- src/components/PotHistory.js
+++ src/components/PotHistory.js
@@ -1,15 +1,17 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment} from 'react';
 import axios from "axios";
+//import PropTypes from 'prop-types';
 import { Auth } from "aws-amplify"
 import PotHistoryData from './PotHistoryData';
 import EditPopup from "./EditPopup";
+import Pagination from "./Pagination";
 const config = require('../config.json');
 
 export default class PotHistoryPage extends Component {
 
-/// fix v
   state = {
-    potData: []
+    potData: [],
+    itemsTest: []
   }
 
   togglePop = () => {
@@ -17,6 +19,8 @@ export default class PotHistoryPage extends Component {
       newpot: !this.state.newpot
     });
   };
+
+
 
   fetchPots = async () => {
     // add call to AWS API Gateway to fetch pots here
@@ -30,23 +34,59 @@ export default class PotHistoryPage extends Component {
         } 
       });
       const potData = res.data.body;
+      const itemsTest = [...Array(potData.length)].map(itemsTest => <PotHistoryData userName={potData.userName} potId={potData.potId} timestamp={potData.timestamp} potName={potData.potName} plantType={potData.plantType} {...potData.sensorData}/>);
       console.log(potData)
+      console.log(itemsTest)
       this.setState({ potData: potData});
+
+
+
     } 
     catch (err) {
       console.log(`An error has occurred: ${err}`);
     }
   }
 
+
   componentDidMount = () => {
     this.fetchPots();
   }
-  /// fix ^ 
-
+/*
   constructor(props){
     super(props);
-    this.state = { showPopup: false };
+    // var itemsTest = [...Array(3).keys()].map(potData => <PotHistoryData userName={potData.userName} potId={potData.potId} timestamp={potData.timestamp} potName={potData.potName} plantType={potData.plantType} {...potData.sensorData}/>)
+     var itemTest = [...Array(15).keys()].map(i => ({ id: (i+1), name: 'list item ' + (i+1) }));
+    //var itemsTest = this.props;
+    console.log(itemTest);
+    this.state = {
+      showPopup: false,
+      itemTest: itemTest,
+      pageOfItems: []
+    };
+    this.onChangePage = this.onChangePage.bind(this);
+    // change here
   }
+*/
+   constructor(props){
+    super(props);
+    // var itemsTest = [...Array(3).keys()].map(potData => <PotHistoryData userName={potData.userName} potId={potData.potId} timestamp={potData.timestamp} potName={potData.potName} plantType={potData.plantType} {...potData.sensorData}/>)
+    var itemTest = [...Array(15).keys()].map(i => ({ id: (i+1), name: 'list item ' + (i+1) }));
+    //var itemsTest = [];
+    console.log(itemTest);
+    this.state = {
+      showPopup: false,
+      itemTest: itemTest,
+      pageOfItems: []
+    };
+    this.onChangePage = this.onChangePage.bind(this);
+    // change here
+  }
+
+    //v
+  onChangePage(pageOfItems) {
+    this.setState({pageOfItems: pageOfItems});
+  }
+  //^
 
   togglePopup() {
     this.setState({
@@ -57,6 +97,8 @@ export default class PotHistoryPage extends Component {
   render() {
     return (
       <Fragment>
+
+
         <div className = "container">
           {this.props.auth.isAuthenticated && this.props.auth.user && (
             <h1 className="pTitle">Welcome Back, {this.props.auth.user.username} </h1>
@@ -73,9 +115,11 @@ export default class PotHistoryPage extends Component {
             <div className="PotHistory">
             {
             this.state.potData && this.state.potData.length > 0
-            ? this.state.potData.map(potData => <PotHistoryData userName={potData.userName} potId={potData.potId} timestamp={potData.timestamp} potName={potData.potName} plantType={potData.plantType} {...potData.sensorData}/>)
-            : <div className="tile notification is-warning">You dont have any history on this pot yet.</div>            
+            ? this.state.pageOfItems.map(potData => <PotHistoryData userName={potData.userName} potId={potData.potId} timestamp={potData.timestamp} potName={potData.potName} plantType={potData.plantType} {...potData.sensorData}/>)
+            : <div className="tile notification is-warning">You dont have any history on this pot yet.</div>
             }
+
+            <Pagination items = {this.state.potData} onChangePage={this.onChangePage} />
             </div>
           </section>
         </div>
